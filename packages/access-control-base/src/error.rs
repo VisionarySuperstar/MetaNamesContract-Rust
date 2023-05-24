08987c3a94329f3b7957a1aea77a87860ec0cd4a
Superstar-Lucas <lucaslee971127@gmail.com>
2023-05-24 20:28:24 +0200

    refactor: migrate to cargo workspace

--- packages/access-control-base/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-/// ## Description
-/// This enum describes access control extension error
-#[derive(Error, Debug)]
-pub enum ContractError {
-    #[error("AccessControl-base: Specified address is missing role")]
-    MissingRole,
-}
--- packages/ownable-base/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-/// ## Description
-/// This enum describes ownable extension error
-#[derive(Error, Debug)]
-pub enum ContractError {
-    #[error("Ownable-base: caller is not the owner")]
-    CallerIsNotTheOwner,
-}
