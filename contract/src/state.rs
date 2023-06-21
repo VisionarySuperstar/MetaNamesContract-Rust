173524b15bd14cd221a37e71cfb1a6e9683192c9
Superstar-Lucas <lucaslee971127@gmail.com>
2023-06-21 11:29:58 +0200

    fix(contract): release build

--- contract/src/state.rs
+++ contract/src/state.rs
@@ -2,6 +2,9 @@ use contract_version_base::state::ContractVersionBase;
 use mpc721_base::state::MPC721ContractState;
 use partisia_name_system::state::PartisiaNameSystemState;
 
+#[allow(unused_imports)]
+use crate::contract::__PBC_IS_ZK_CONTRACT;
+
 #[state]
 #[derive(PartialEq, Eq, Debug)]
 pub struct ContractState {
