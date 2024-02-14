b614679f25856b3aaa4cfbc8fc33624ebee27aa3
Superstar-Lucas <lucaslee971127@gmail.com>
2024-02-14 12:53:40 +0100

    Add proxy contract (#42)
    * feat: add contract proxy
    
    * fix(proxy): contract
    
    * feat(CI): add proxy_contract to build contract
    
    * feat(CI): update image to 1.74
    
    * feat(CI): update rust version to 1.74
    
    * refactor(proxy): imports
    
    * fix(CI): build artifacts

---
+++ contract-proxy/README.md
@@ -0,0 +1,2 @@
+# Contract proxy
+Introduce a contract proxy to streamline the deployment process. This proxy maintains the address of the target contract.
---
+++ contract-proxy/src/lib.rs
@@ -0,0 +1,58 @@
+#![doc = include_str!("../README.md")]
+
+#[macro_use]
+extern crate pbc_contract_codegen;
+extern crate contract_version_base;
+extern crate pbc_contract_common;
+
+use contract_version_base::state::ContractVersionBase;
+
+use pbc_contract_common::{address::Address, context::ContractContext};
+
+const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+/// State of the contract
+#[state]
+struct ContractState {
+    pub address: Address,
+    pub version: ContractVersionBase,
+}
+
+/// Initialize a new Nickname contract.
+///
+/// # Arguments
+///
+/// * `_ctx` - the contract context containing information about the sender and the blockchain.
+///
+/// # Returns
+///
+/// The initial state of the contract
+#[init]
+fn initialize(_ctx: ContractContext, address: Address) -> ContractState {
+    ContractState {
+        address,
+        version: ContractVersionBase::new(CONTRACT_NAME, CONTRACT_VERSION),
+    }
+}
+
+/// Update contract state
+///
+/// # Arguments
+///
+/// * `_ctx` - the contract context containing information about the sender and the blockchain.
+/// * `state` - the current state of the contract
+/// * `address` - new contract address
+///
+/// # Returns
+/// Updated contract state
+#[action(shortname = 0x01)]
+fn update_address(
+    _ctx: ContractContext,
+    mut state: ContractState,
+    address: Address,
+) -> ContractState {
+    state.address = address;
+
+    state
+}
